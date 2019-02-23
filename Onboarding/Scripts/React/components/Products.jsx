f9577a044fe8504accad7426af771cff8b31b2a1
David <david.7stars@gmail.com>
2019-02-24 04:26:59 +1300

    Fixed table showed blank data after deleting.

---
+++ Onboarding/Scripts/React/components/Products.jsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import ListingTable from './ListingTable';
+
+class Products extends React.Component {
+  render() {
+    const columnHead = [
+      { name: 'Name', type: 'text' },
+      { name: 'Price', type: 'money' },
+    ];
+    const populateHead = null;
+    const serviceName = "Product";
+    return (
+      <ListingTable serviceName={serviceName} columnHead={columnHead} populateHead={populateHead} />
+    );
+  }
+}
+
+export default Products;
