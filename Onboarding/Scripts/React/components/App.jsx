4514c4fa35667dabe856a961ac0f41e8ba38285f
David <david.7stars@gmail.com>
2019-02-25 10:27:52 +1300

    Extracted Navbar component

---
+++ Onboarding/Scripts/React/components/App.jsx
@@ -0,0 +1,87 @@
+﻿import React from 'react';
+import Navbar from './Navbar';
+import ListingTable from './ListingTable';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      serviceName: 'Customer',
+      columnHead: [
+        { name: 'Name', type: 'text' },
+        { name: 'Address', type: 'text' },
+      ],
+      populateHead: null,
+    };
+    this.selectedService = this.selectedService.bind(this);
+    this.menuItems = ['Customers', 'Products', 'Stores', 'Sales'];
+  }
+
+  selectedService(name) {
+    let columnHead, populateHead, serviceName = null;
+    switch (name) {
+      case 'Products':
+        serviceName = "Product";
+        populateHead = null;
+        columnHead = [
+          { name: 'Name', type: 'text' },
+          { name: 'Price', type: 'money' },
+        ];
+        break;
+      case 'Stores':
+        serviceName = "Store";
+        populateHead = null;
+        columnHead = [
+          { name: 'Name', type: 'text' },
+          { name: 'Address', type: 'text' },
+        ];
+        break;
+      case 'Sales':
+        serviceName = "Sales";
+        columnHead = [
+          { name: 'Customer', type: 'text' },
+          { name: 'Product', type: 'text' },
+          { name: 'Store', type: 'text' },
+          { name: 'DateSold', type: 'date' },
+        ];
+        populateHead = [
+          { name: 'DateSold', type: 'date' },
+          { name: 'CustomerId', type: 'dropdown' },
+          { name: 'ProductId', type: 'dropdown' },
+          { name: 'StoreId', type: 'dropdown' },
+        ];
+        break;
+      default:
+        serviceName = "Customer";
+        populateHead = null;
+        columnHead = [
+          { name: 'Name', type: 'text' },
+          { name: 'Address', type: 'text' },
+        ];
+    }
+    this.setState({ serviceName, columnHead, populateHead });
+  }
+
+  render() {
+    const { serviceName, columnHead, populateHead } = this.state;
+    return (
+      <React.Fragment>
+
+        <Navbar
+          header="React"
+          onSelectedItem={this.selectedService}
+          menuItems={this.menuItems}
+        />
+
+        <ListingTable
+          serviceName={serviceName}
+          columnHead={columnHead}
+          populateHead={populateHead}
+        />
+
+      </React.Fragment >
+    )
+  }
+}
+
+export default App;
---
+++ Onboarding/Scripts/React/components/Navbar.jsx
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { Menu } from 'semantic-ui-react';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeItem: this.props.menuItems[0] };
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick(e, { name }) {
+    this.setState({ activeItem: name });
+    this.props.onSelectedItem(name);
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <Menu inverted>
+          <Menu.Item header><h2>{this.props.header}</h2></Menu.Item>
+          {
+            this.props.menuItems.map(item =>
+              <Menu.Item key={item} name={item}
+                active={this.state.activeItem === item} onClick={this.handleItemClick} />
+            )
+          }
+        </Menu>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Navbar;
